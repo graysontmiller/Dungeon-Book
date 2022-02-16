48fdb12ca4ef311c0337a3b709e3681f936563a3
Veoprato <75872616+Veoprato@users.noreply.github.com>
2022-02-16 17:40:33 -0500

    basic routes structure added

---
+++ controllers/home-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+// const { Post, User, Comment } = require('../models');
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+// router.get('/register', (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect('/');
+//     return;
+//   }
+
+//   res.render('register');
+// });
+
+module.exports = router;
