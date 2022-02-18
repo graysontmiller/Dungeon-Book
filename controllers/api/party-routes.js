ea11d66847db31fdc98a98982af881cb667322e3
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2022-02-18 17:35:34 -0500

    created route to find specific party by id

--- controllers/api/party-routes.js
+++ controllers/api/party-routes.js
@@ -1,2 +1,39 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Party } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Party.findAll({
+      attributes: [
+        'party_name',
+        'party_pass',
+        'character_id',
+        'user_id',
+      ]
+    })
+      .then(dbPartyData => res.json(dbPartyData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  // get a specific party
+router.get('/:id', (req, res) => {
+    Party.findOne({
+        attributes: {exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPartyData => res.json(dbPartyData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
