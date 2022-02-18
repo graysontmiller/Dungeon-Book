879601d628a11faaaedf9bf2eb62cc9b44d8bef2
Veoprato <75872616+Veoprato@users.noreply.github.com>
2022-02-17 22:23:49 -0500

    added in require('express').Router(); for future use

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Character, Stats , PClass , Hp , Combat , Party } = require('../models');
 
+router.get('/', (req, res) => {
+  console.log(req.session);
+  res.render('homepage')
+});
+
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
