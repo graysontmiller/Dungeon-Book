23cf1b3b7bebc9c9d1310eda5fa2aae41b5fc4de
Veoprato <75872616+Veoprato@users.noreply.github.com>
2022-02-20 18:02:03 -0500

    test endpoints created for parties api

--- controllers/api/party-routes.js
+++ controllers/api/party-routes.js
@@ -1,32 +1,40 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection');
 const { User, Character , Party , PartyGM , UserChar , UserCharParty  } = require('../../models');
-// const withAuth = require('../../utils/auth');
 
-// get all parties
 router.get('/', (req, res) => {
-    console.log('======================');
     Party.findAll()
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+router.get('/test/:id', (req, res) => {
+    Party.findAll({
+        where: {
+            GM_id: req.params.id
+        },
+        include:'GM'
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+router.post('/', (req, res) => {
+    Party.create({
+        party_name: req.body.party_name,
+        party_pass: req.body.party_pass,
+        GM_id: req.body.GM_id
+    })
       .then(dbPartyData => res.json(dbPartyData))
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
-
-  // get a specific party
-router.get('/:id', (req, res) => {
-    Party.findOne({
-        attributes: {exclude: ['password'] },
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(dbPartyData => res.json(dbPartyData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
 });
 
 module.exports = router;
